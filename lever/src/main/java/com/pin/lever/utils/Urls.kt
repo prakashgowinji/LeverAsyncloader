9b09d95731de7cc13a43dfaae143140034b1e1e1
prakashgowinji <prakashgowinji@gmail.com>
2019-08-18 14:13:28 +0530

    Moved BaseUrl to lever:app:gradle

--- lever/src/main/java/com/pin/lever/utils/Urls.kt
+++ lever/src/main/java/com/pin/lever/utils/Urls.kt
@@ -1,8 +1,10 @@
 package com.pin.lever.utils
 
+import com.pin.lever.BuildConfig
+
 class Urls {
     companion object{
-        const val BASE_URL = "http://pastebin.com/"
+        const val BASE_URL = BuildConfig.BASE_URL
         const val GET_INFO = "raw/wgkJgazE"
     }
 }
